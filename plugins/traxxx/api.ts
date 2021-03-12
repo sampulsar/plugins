5ea434505f67cee9f6b3d386c84bae6c28313dbb
SamPulsar <sampulsar1@gmail.com>
2021-03-13 05:56:19 +1030

    Add Actors and Scene API definitions

--- plugins/traxxx/api.ts
+++ plugins/traxxx/api.ts
@@ -26,6 +26,68 @@ export namespace EntityResult {
   }
 }
 
+export namespace ActorResult {
+  export interface Actor {
+    id: number;
+		name: string;
+    gender?: string;
+    aliasFor?: string;
+    dateOfBirth?: string;
+    birthCountry?: string;
+    placeOfBirth?: { country: { name: string; alias: string } }, 
+    alias?: string[];
+    dateOfDeath?: string;
+    cup?: string;
+    bust?: number;
+    waist?: number;
+    hip?: number;
+    naturalBoobs?: boolean;
+    height?: number;
+    weight?: number;
+    eyes?: string;
+    hairColor?: string;
+    hasTattoos?: boolean;
+    hasPiercings?: boolean;
+    tattoos?: string;
+    piercings?: string;
+    ethnicity?: string;
+    age?: number;
+    avatar?: { id: string; path?: string; };
+  }
+
+  export interface Result {
+    // Entity always exists for successful http requests
+    // ex: null when 404
+    actor: Actor;
+    actors: Actor[];
+  }
+
+}
+
+export namespace SceneResult {
+  export interface Scene {
+    relevance: number;
+    id: number;
+    entryId: string;
+    shootId: string;
+    title: string;
+    url: string;
+    date: string;
+    description: string | null;
+    entity: EntityResult.Entity;
+    actors: ActorResult.Actor[];
+    tags: [{ name: string }];
+    poster: { path: string; };
+  }
+
+  export interface Result {
+    // Entity always exists for successful http requests
+    // ex: null when 404
+    scene: Scene;
+    scenes: Scene[];
+  }
+}
+
 export class Api {
   ctx: Context;
   axios: AxiosInstance;
@@ -49,6 +111,34 @@ export class Api {
    */
   public async getNetwork(idOrSlug: string | number): Promise<AxiosResponse<EntityResult.Result>> {
     return this.axios.get<EntityResult.Result>(`/networks/${idOrSlug}`);
+  }
+
+  /**
+   * @param query - query to find the scene
+   */
+  public async getActors(query: string): Promise<AxiosResponse<ActorResult.Result>> {
+    return this.axios.get<ActorResult.Result>(`/actors?limit=3&q=${query}`);
+  }
+
+  /**
+   * @param id - the id of the scene
+   */
+  public async getActor(idOrSlug: string | number): Promise<AxiosResponse<ActorResult.Result>> {
+    return this.axios.get<ActorResult.Result>(`/actors/${idOrSlug}`);
+  }
+
+  /**
+   * @param query - query to find the scene
+   */
+  public async getScenes(query: string): Promise<AxiosResponse<SceneResult.Result>> {
+    return this.axios.get<SceneResult.Result>(`/scenes?limit=2&q=${query}`);
+  }
+
+  /**
+   * @param id - the id of the scene
+   */
+  public async getScene(id: number): Promise<AxiosResponse<SceneResult.Result>> {
+    return this.axios.get<SceneResult.Result>(`/scenes/${id}`);
   }
 
   /**
